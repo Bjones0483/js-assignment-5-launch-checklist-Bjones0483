64387b0196eb149a7b32beac982d6d90d781190f
Bruce Jones <brucedjonesjr@gmail.com>
2022-12-05 20:50:40 -0600

    script.js

--- script.js
+++ script.js
@@ -1,65 +1,75 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    const document = window.document
+    const pilot = document.querySelector("input[name=pilotName]");
+    const copilot = document. querySelector("input[name=copilotName]");
+    const fuelLevel = document.querySelector("input[name=fuelLevel]");
+    const cargoLevel = document.querySelector("input[name=cargoMass]");
+    const list = document.getElementById("faultyItems"); 
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
+    list.style.visibility = "hidden";   
 
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
-});
-fetch("https://handlers.education.launchcode.org/static/planets.json")
-.then((res) => {
-   res.json().then((json) => {
-      let index = Math.floor(Math.random() * json.length);
-        console.log(json[index].name);
-   
-        document.getElementById("missionTarget").innerHTML = `
-        <h2>Mission Destination</h2>
-        <ol>
-           <li>Name: ${json[index].name}</li>
-           <li>Diameter: ${json[index].diameter}</li>
-           <li>Star: ${json[index].star}</li>
-           <li>Distance from Earth: ${json[index].distance}</li>
-           <li>Number of Moons: ${json[index].moons}</li>
-        </ol>
-        <img src="${json[index].image}">
-        `;
-     });
-  });
+     
+    let form = document.querySelector("form"); 
+    form.addEventListener("submit", function(event) { 
+        
+        formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
+        
+        if (validateInput(pilot.value) == "Empty" || validateInput(copilot.value) == "Empty" || validateInput(fuelLevel.value) == "Empty" || validateInput(cargoLevel.value) == "Empty") {
+            list.style.visibility = "hidden"; 
+            alert("All fields are required!");
+            event.preventDefault();
+        } 
+        if (validateInput(pilot.value) == "Is a Number" || validateInput(copilot.value) == "Is a Number") {
+            list.style.visibility = "hidden"; 
+            alert("Pilot and CoPilot's names should be letters only.");
+            event.preventDefault();
+        }
 
-  document.querySelector("form").addEventListener("submit", (event) =>{
-    event.preventDefault();
-
-    // Inputs
-    let pilotName   = document.querySelector("input[name=pilotName]");
-    let copilotName = document.querySelector("input[name=copilotName]");
-    let fuelLevel   = document.querySelector("input[name=fuelLevel]");
-    let cargoMass   = document.querySelector("input[name=cargoMass]");
+        if (validateInput(fuelLevel.value) == "Not a Number" || validateInput(cargoLevel.value) == "Not a Number") {
+            list.style.visibility = "hidden"; 
+            alert("Fuel Level and Cargo Mass should be numbers only.");
+            event.preventDefault();
+        }
+        event.preventDefault();
+    }) 
     
-    // Outputs
-    let pilotStatus   = document.getElementById("pilotStatus");
-    let copilotStatus = document.getElementById("copilotStatus");
-    let fuelStatus    = document.getElementById("fuelStatus");
-    let cargoStatus   = document.getElementById("cargoStatus");
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        // Pick a planet from the list of planets and add that information to your destination.
+        let pickedPlanet = pickPlanet(listedPlanets);
 
-    // Other stuff
-    let faultyItems  = document.getElementById("faultyItems");
-    let launchStatus = document.getElementById("launchStatus");
+        addDestinationInfo(window.document, pickedPlanet.name, pickedPlanet.diameter, pickedPlanet.star, pickedPlanet.distance, pickedPlanet.moons, pickedPlanet.image);
+    })    
 
-    // Update status
-    pilotStatus.innerText   = `Pilot ${pilotName.value} Ready`;
-    copilotStatus.innerText = `Co-Pilot ${copilotName.value} Ready`;
-    fuelStatus.innerText    = `Fuel Level high enough for launch`;
-    cargoStatus.innerText   = `Cargo Level high enough for launch`; 
 });
-
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
    
    
